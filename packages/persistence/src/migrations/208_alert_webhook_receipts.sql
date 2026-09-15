012d2e9754ead9a5d2a833e1cc89b7aa30aa3254
gblw <hypjaap@gmail.com>
2026-09-15 09:11:25 +0800

    chore: capture latest acceptance evidence

--- packages/persistence/src/migrations/208_alert_webhook_receipts.sql
+++ packages/persistence/src/migrations/208_alert_webhook_receipts.sql
@@ -14,8 +14,7 @@ CREATE TABLE alert_webhook_receipts (
 REVOKE ALL ON alert_webhook_receipts FROM PUBLIC;
 DO $$ BEGIN
   IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'merchant_ops') THEN
-    GRANT SELECT, INSERT ON alert_webhook_receipts TO merchant_ops;
-    REVOKE UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER ON alert_webhook_receipts FROM merchant_ops;
+    REVOKE ALL PRIVILEGES ON alert_webhook_receipts FROM merchant_ops;
   END IF;
 END $$;
 
